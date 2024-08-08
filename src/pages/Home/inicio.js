e4248273179bbe751a977ec8fccd2a8462579ed2
code-explorer73 <118012243+code-explorer73@users.noreply.github.com>
2024-08-08 18:17:07 -0300

    falhou de novo
    deu errado, tentei fazer o carrossel

---
+++ src/pages/Home/inicio.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { SafeAreaView, Text, View, StyleSheet, ScrollView } from 'react-native';
+
+function inicio() {
+    <ScrollView 
+scrollEventThrottle={16}
+>
+<View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}>
+<Text style={{ fontSize: 24, fontWeight: '700',
+paddingHorizontal: 20}}>
+On Live
+</Text>
+
+
+
+
+
+</View>
+</ScrollView>
+}
+
+export default inicio;
