5f602758b3f517e1d181cb194659695e5c715285
FelipePaterni <118012333+FelipePaterni@users.noreply.github.com>
2024-08-23 13:35:18 -0300

    Create index.js

---
+++ src/pages/Recuperacao/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+
+export default function Recuperacao() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.caixa1}>
+
+                <View style={{ paddingBottom: 5, borderBottomWidth: 2, borderColor: "#40173D", height: "100%", marginRight: 10, justifyContent: "flex-end" }}>
+                    <Text style={{ color: "black", fontSize: 25, fontWeight: "bold" }}>Recuperar senha</Text>
+                </View>
+
+            </View>
+            <View style={styles.caixa2}>
+
+                <View style={styles.boxInput}>
+                    <Text style={styles.inpTitulo}>Email</Text>
+                    <TextInput style={styles.input} placeholder='Digite o seu email' keyboardType='email-address' autoComplete='email' placeholderTextColor='#A481A1' />
+                </View>
+            </View>
+
+
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff",
+        flex: 1,
+
+    },
+    caixa1: {
+        //backgroundColor: "blue",
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    caixa2: {
+        backgroundColor: "blue",
+        flex: 5,
+    },
+
+    text: {
+        alignSelf: "center",
+        margin: 10,
+        marginBottom: 25,
+        color: "#40173D",
+        fontSize: 20,
+        lineHeight: 25,
+        fontWeight: "normal",
+        fontFamily: "OpenSansBold"
+    },
+
+    boxInput: {
+
+    },
+
+    inpTitulo:{
+
+    },
+    input:{
+        
+    }
+});
