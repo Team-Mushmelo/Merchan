42a2e2ed7b79a0ea4c701c5ec30c5aa3fee38849
MurielTX <118012110+MurielTX@users.noreply.github.com>
2024-08-28 17:56:22 -0300

    alteração do conflito
    solução

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -80,10 +80,12 @@ const styles = StyleSheet.create({
         padding: 20,
     },
     formContainer: {
-        backgroundColor: '#dadada',
+
+        backgroundColor: '#ebe8e2',
         borderColor: '#40173d',
         borderWidth: 1,
         borderRadius: 50,
+        height: 500,
         flex: 1,
         justifyContent: 'center',
     },
@@ -96,6 +98,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
     },
     input: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#000',
         flex: 1,
         height: 40,
         fontSize: 16,
