1d28d6b1eb53972e651085d0b90c85da0ece7238
FelipePaterni <118012333+FelipePaterni@users.noreply.github.com>
2024-09-03 23:34:16 -0300

    Revert "Update App.js"
    This reverts commit a66eb20657c0a6aaa4065fb65ceed8db40ff60a6.

--- App.js
+++ App.js
@@ -2,22 +2,20 @@ import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar, SafeAreaView, Image } from 'react-native';
-
 import Logo from './assets/splash.png';
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold, } from '@expo-google-fonts/open-sans';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 
-//paginas principais
+// Páginas principais
 import Routes from './src/pages/Routes';
 import PaginaLogar from './src/pages/PaginaLogar/';
 import Modo from './src/pages/Modo';
 import Recuperacao from './src/pages/Recuperacao';
 import Preferencias from './src/pages/Preferencias';
-import FinalLogin from './src/pages/Final_Login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [isLogado, setIsLogado] = useState(false);
+  const [user, setUser] = useState(false);
 
   let [fontsLoaded] = useFonts({
     'OpenSansRegular': OpenSans_400Regular,
@@ -26,34 +24,31 @@ export default function App() {
 
   if (!fontsLoaded) {
     return (
-      <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItem: "center", backgroundColor: "#BE00B0" }}>
-        <Image source={Logo} style={{ width: '100%', height: "100%" }} />
+      <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#BE00B0" }}>
+        <Image source={Logo} style={{ width: '100%', height: '100%' }} resizeMode="cover" />
       </SafeAreaView>
     );
-  } else {
-    return (
-      <>
-        <StatusBar translucent backgroundColor={'#00000000'} />
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            {!isLogado ? (
-              <>
-            
-               
-                <Stack.Screen name='Modo' component={Modo} />
-                <Stack.Screen name="Recuperacao" component={Recuperacao} />
-                <Stack.Screen name="PaginaLogar">
-                  {(props) => <PaginaLogar {...props} setIsLogado={setIsLogado} />}
-                </Stack.Screen>
-                <Stack.Screen name="Preferencias" component={Preferencias} />
-                <Stack.Screen name="FinalLogin" component={FinalLogin}/>
-              </>
-            ) : (
-              <Stack.Screen name="MeuApp" component={Routes} />
-            )}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </>
-    );
   }
+
+  return (
+    <>
+      <StatusBar translucent backgroundColor={'#00000000'} />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {!user ? (
+            <>
+              <Stack.Screen name='Modo' component={Modo} />
+              <Stack.Screen name='Preferencias' component={Preferencias} />
+              <Stack.Screen name='Recuperacao' component={Recuperacao} />
+              <Stack.Screen name='PaginaLogar'>
+                {(props) => <PaginaLogar {...props} setUser={setUser} />}
+              </Stack.Screen>
+            </>
+          ) : (
+            <Stack.Screen name='MeuApp' component={Routes} />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
