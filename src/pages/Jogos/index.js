36c791e01938a0ffea0e92df2742cf6f505df79c
MurielTX <murilorenan777@outlook.com>
2024-10-12 16:25:30 -0300

    pesquisas
    C&M

--- src/pages/Jogos/index.js
+++ src/pages/Jogos/index.js
@@ -1,23 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Animated, Modal, KeyboardAvoidingView } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Animated, KeyboardAvoidingView } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
 
+const WORDS = {
+   'Aventura Espacial': ["galáxia", "nave", "astronauta", "cometa", "buraco negro", "estrela", "planeta", "satélite", "foguete", "exploração"],
+'Fantasia Medieval': ["cavaleiro", "castelo", "dragão", "espada", "reino", "rainha", "mago", "taverna", "aventura", "batalha"],
+'Pós-Apocalíptico': ["sobrevivente", "deserto", "mutante", "ruínas", "catástrofe", "refúgio", "fome", "pandemia", "guerra", "desolação"],
+};
+
+const THEMES = [
+    { name: 'Aventura Espacial', icon: 'rocket' },
+    { name: 'Fantasia Medieval', icon: 'star' },
+    { name: 'Pós-Apocalíptico', icon: 'warning' },
+];
+
 const App = () => {
+    const [word, setWord] = useState('');
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    const [wrongGuesses, setWrongGuesses] = useState(0);
     const [message, setMessage] = useState('');
     const [currentTheme, setCurrentTheme] = useState({ name: '', icon: '' });
     const [isGameActive, setIsGameActive] = useState(false);
-    const [timeLeft, setTimeLeft] = useState(60);
+    const [timeLeft, setTimeLeft] = useState(0);
     const [score, setScore] = useState(0);
-    const [isModalVisible, setIsModalVisible] = useState(false);
     const [timer, setTimer] = useState(30);
+    const [gameOver, setGameOver] = useState(false);
     const maxWrongGuesses = 6;
     const symbols = ['O', '-', ']', '-', '-', '<', ' '];
     const [firstWrongGuess, setFirstWrongGuess] = useState(true);
-    
-    const [titleAnimation] = useState(new Animated.Value(-50));
-    const [contentAnimation] = useState(new Animated.Value(-50));
-    const [opacityAnimation] = useState(new Animated.Value(0));
+
+    // Animações
+    const titleAnimation = useState(new Animated.Value(-50))[0];
+    const contentAnimation = useState(new Animated.Value(-50))[0];
+    const opacityAnimation = useState(new Animated.Value(0))[0];
 
     useEffect(() => {
         let timer;
@@ -35,40 +51,50 @@ const App = () => {
             interval = setInterval(() => {
                 setTimer((prev) => prev - 1);
             }, 1000);
+        } else if (timer === 0) {
+            endGame(false);
         }
         return () => clearInterval(interval);
     }, [isGameActive, timer]);
 
     const handleStartGame = () => {
         const randomTheme = THEMES[Math.floor(Math.random() * THEMES.length)];
+        setCurrentTheme(randomTheme);
+
+        const randomWord = WORDS[randomTheme.name][Math.floor(Math.random() * WORDS[randomTheme.name].length)];
+        setWord(randomWord);
+        setGuessedLetters([]);
+        setWrongGuesses(0);
         setMessage('');
         setFirstWrongGuess(true);
         setIsGameActive(true);
-        setTimeLeft(60);
+        setGameOver(false);
+        setTimeLeft(30);
         setScore(0);
         setTimer(30);
 
+        // Animações
         Animated.parallel([
             Animated.timing(titleAnimation, {
                 toValue: 0,
-                duration: 500,
+                duration: 1000,
                 useNativeDriver: true,
             }),
             Animated.timing(contentAnimation, {
                 toValue: 0,
-                duration: 500,
+                duration: 1000,
                 useNativeDriver: true,
             }),
             Animated.timing(opacityAnimation, {
                 toValue: 1,
-                duration: 500,
+                duration: 1000,
                 useNativeDriver: true,
             }),
         ]).start();
     };
 
     const handleGuess = (letter) => {
-        if (!isGameActive || guessedLetters.includes(letter)) return;
+        if (!isGameActive || guessedLetters.includes(letter) || wrongGuesses >= maxWrongGuesses) return;
 
         setGuessedLetters([...guessedLetters, letter]);
 
@@ -76,9 +102,9 @@ const App = () => {
             setTimeLeft((prev) => prev + 10);
             if (word.split('').every((char) => guessedLetters.includes(char) || char === letter)) {
                 setScore((prev) => prev + 25);
-                setMessage('Você ganhou! O-]--<');
+                setMessage('Você ganhou!');
                 setIsGameActive(false);
-                setIsModalVisible(true);
+                setGameOver(true);
             }
             setTimer((prev) => prev + 5);
         } else {
@@ -87,75 +113,81 @@ const App = () => {
             } else {
                 setWrongGuesses(wrongGuesses + 1);
             }
-            if (wrongGuesses + (firstWrongGuess ? 0 : 1) >= maxWrongGuesses) {
-                setMessage(`Fim de jogo! A palavra era: ${word}`);
-                setIsGameActive(false);
-                setIsModalVisible(true);
-            }
+        }
+
+        if (wrongGuesses + (firstWrongGuess ? 0 : 1) >= maxWrongGuesses) {
+            endGame(true);
         }
     };
 
-    const calculateScore = () => {
-        return guessedLetters.reduce((acc, letter) => {
-            return word.includes(letter) ? acc + 5 : acc;
-        }, 0);
-    };
-
     const endGame = (isLost) => {
-        setMessage(isLost ? `Fim de jogo! A palavra era: ${word}` : 'Tempo esgotado! A palavra era: ' + word);
+        setMessage(isLost ? `Fim de jogo! A palavra era: ${word}` : 'Você ganhou!');
         setIsGameActive(false);
-        setScore(calculateScore());
+        setGameOver(true);
     };
 
     const renderWord = () => {
-        // Implementar a lógica de renderização da palavra
+        return word.split('').map((letter, index) => (
+            <Text key={index} style={styles.letter}>
+                {guessedLetters.includes(letter) ? letter : '_'}
+            </Text>
+        ));
     };
 
-    const handleCloseModal = () => {
-        setIsModalVisible(false);
+    const renderAlphabet = () => {
+        const alphabet = 'abcdefghijklmnopqrstuvwxyzçâôáéíóú';
+        return alphabet.split('').map((letter) => (
+            <TouchableOpacity
+                key={letter}
+                onPress={() => handleGuess(letter)}
+                disabled={!isGameActive || guessedLetters.includes(letter) || wrongGuesses >= maxWrongGuesses}
+                style={[styles.button, { backgroundColor: isGameActive ? (guessedLetters.includes(letter) ? '#e0e0e0' : '#bf0cb1') : '#d3d3d3' }]} >
+                <Text style={styles.buttonText}>{letter}</Text>
+            </TouchableOpacity>
+        ));
+    };
+
+    const renderHangman = () => {
+        const currentSymbol = symbols.slice(0, wrongGuesses).join(' ');
+        return <Text style={styles.hangman}>{currentSymbol}</Text>;
+    };
+
+    const handleRestartGame = () => {
         handleStartGame();
     };
 
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Animated.Text style={[styles.title, { transform: [{ translateY: titleAnimation }], opacity: opacityAnimation, color: '#40173d' }]}>
-                Jogo da Forca
-            </Animated.Text>
-            <Animated.Text style={[styles.subtitle, { transform: [{ translateY: titleAnimation }], opacity: opacityAnimation, color: '#40173d' }]}>
-                seu primeiro erro é perdoado
-            </Animated.Text>
-            <Text style={styles.timer}>Tempo: {timeLeft}s</Text>
-            <Text style={styles.score}>Pontuação: {score}</Text>
-            {currentTheme.name && (
-                <Animated.View style={[styles.themeContainer, { transform: [{ translateY: contentAnimation }], opacity: opacityAnimation }]}>
-                    <Entypo name={currentTheme.icon} size={32} style={[styles.themeIcon, { color: '#40173d' }]} />
-                    <Text style={[styles.themeName, { fontFamily: 'BungeeRegular', color: '#40173d' }]}>{currentTheme.name}</Text>
-                </Animated.View>
-            )}
-            <View style={styles.hangmanContainer}>
-                {renderHangman()}
-            </View>
-            <Animated.View style={{ transform: [{ translateY: contentAnimation }], opacity: opacityAnimation }}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+            <ScrollView contentContainerStyle={styles.scrollViewContainer} keyboardShouldPersistTaps="handled">
+                <Animated.Text style={[styles.title, { transform: [{ translateY: titleAnimation }], opacity: opacityAnimation }]}>
+                    Jogo da Forca
+                </Animated.Text>
+                <Animated.Text style={[styles.subtitle, { transform: [{ translateY: titleAnimation }], opacity: opacityAnimation }]}>
+                    seu primeiro erro é perdoado
+                </Animated.Text>
+                <Text style={styles.timer}>Tempo: {timeLeft}s</Text>
+                <Text style={styles.score}>Pontuação: {score}</Text>
+                {currentTheme.name && (
+                    <Animated.View style={[styles.themeContainer, { transform: [{ translateY: contentAnimation }], opacity: opacityAnimation }]}>
+                        <Entypo name={currentTheme.icon} size={32} style={styles.themeIcon} />
+                        <Text style={styles.themeName}>{currentTheme.name}</Text>
+                    </Animated.View>
+                )}
+                <View style={styles.hangmanContainer}>
+                    {renderHangman()}
+                </View>
                 <View style={styles.wordContainer}>
                     {renderWord()}
                 </View>
-            </Animated.View>
-            <TouchableOpacity style={styles.startButton} onPress={handleStartGame}>
-                <Ionicons name="play" size={30} color="#fff" />
-            </TouchableOpacity>
-
-            <Modal visible={isModalVisible} transparent={true} animationType="slide">
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.modalTitle}>Fim de jogo!</Text>
-                        <Text style={styles.modalScore}>Sua pontuação: {score}</Text>
-                        <TouchableOpacity style={styles.closeButton} onPress={handleCloseModal}>
-                            <Text style={styles.closeButtonText}>Recomeçar</Text>
-                        </TouchableOpacity>
-                    </View>
+                <Text style={styles.message}>{message}</Text>
+                <View style={styles.alphabetContainer}>
+                    {renderAlphabet()}
                 </View>
-            </Modal>
-        </ScrollView>
+                <TouchableOpacity style={styles.startButton} onPress={handleStartGame}>
+                    <Ionicons name="play" size={30} color="#fff" />
+                </TouchableOpacity>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
@@ -165,16 +197,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#f9f9f9',
     },
     scrollViewContainer: {
-        flexGrow: 1,
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
-        backgroundColor: '#f9f9f9',
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
+        marginBottom: 10,
+        fontFamily: 'BungeeRegular',
+        color: '#40173d',
+    },
+    subtitle: {
+        fontSize: 12,
+        fontWeight: 'bold',
         marginBottom: 20,
+        color: '#40173d',
     },
     timer: {
         fontSize: 18,
@@ -189,49 +227,74 @@ const styles = StyleSheet.create({
     themeContainer: {
         flexDirection: 'row',
         alignItems: 'center',
+        marginBottom: 20,
+    },
+    themeIcon: {
+        marginRight: 10,
+    },
+    themeName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#40173d',
     },
     hangmanContainer: {
-        // Definir estilos para o contêiner do boneco da forca
+        marginBottom: 20,
     },
-    modalContainer: {
-        flex: 1,
+    hangman: {
+        fontSize: 48,
+        textAlign: 'center',
+        lineHeight: 40,
+    },
+    wordContainer: {
+        flexDirection: 'row',
+        marginBottom: 20,
+    },
+    letter: {
+        fontSize: 32,
+        margin: 5,
+    },
+    message: {
+        fontSize: 18,
+        marginBottom: 20,
+        color: '#40173d',
+        textAlign: 'center',
+    },
+    alphabetContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
         justifyContent: 'center',
+        marginBottom: 20,
+    },
+    button: {
+        borderRadius: 50,
+        width: 50,
+        height: 50,
         alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        margin: 5,
+        elevation: 4,
+        shadowColor: '#40173d',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.4,
+        shadowRadius: 4,
     },
-    modalContent: {
-        width: 300,
-        padding: 20,
-        backgroundColor: '#fff',
-        borderRadius: 10,
+    buttonText: {
+        fontSize: 24,
+        color: '#fff',
     },
-    scoreContainer: {
+    startButton: {
+        backgroundColor: '#bf0cb1',
+        width: 60,
+        height: 60,
+        borderRadius: 30,
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: 20,
-    },
-    modalTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    modalScore: {
-        fontSize: 18,
-        marginBottom: 20,
-    },
-    closeButton: {
-        backgroundColor: '#bf0cb1',
-        padding: 10,
-        borderRadius: 5,
-    },
-    closeButtonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-    },
-    score: {
-        fontSize: 24,
-        color: '#40173d',
-        textAlign: 'center',
+        elevation: 4,
+        shadowColor: '#40173d',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.4,
+        shadowRadius: 4,
     },
 });
 
